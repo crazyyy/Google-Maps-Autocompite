5b192c8f9cee4342b6a4254a4b78a8fa7bd672fa
Vitalii Antoniuk <vitalii@litnevski.studio>
2019-12-06 19:24:10 +0200

    init

---
+++ tasks/build-js-vendors.js
@@ -0,0 +1,25 @@
+/**
+ * Build js vendor (concatenate vendors array)
+ */
+'use strict';
+
+const gulp = require('gulp');
+const filesExist = require('files-exist');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+
+module.exports = function (options) {
+
+  return (done) => {
+    let jsVendors = require(`../${options.src}/vendor_entries/${options.vendorJs}`);
+
+    if (jsVendors.length == 0) {
+      return done();
+    }
+
+    return gulp.src(filesExist(jsVendors))
+      .pipe(concat(options.vendorJsMin))
+      .pipe(uglify())
+      .pipe(gulp.dest(`./${options.dest}/js`));
+  };
+};
---
+++ tasks/watch.js
@@ -0,0 +1,34 @@
+/**
+ * Watch for file changes
+ */
+'use strict';
+
+const gulp = require('gulp');
+
+module.exports = function (options) {
+  const { files } = options.sassFilesInfo;
+
+  return () => {
+    gulp.watch(`./${options.src}/js/**/*`, gulp.series(options.tasks.buildCustomJs, options.tasks.esLint));
+
+    gulp.watch(`./${options.src}/scss/**/*`, gulp.series(options.tasks.buildSass));
+
+    gulp.watch(`./${options.src}/html/**/*`, gulp.series(options.tasks.fileInclude, options.tasks.htmlHint));
+
+    gulp.watch(`./${options.src}/images/**/*.+(${options.imageExtensions})`)
+      .on('unlink', (path) => {
+        options.deleteFile({
+          path,
+          event: 'unlink'
+        }, options.src, options.dest);
+      })
+      .on('add', gulp.series(options.tasks.imageMin));
+
+    gulp.watch([`./${options.dest}/**/*`, `!./${options.dest}/**/*.map`, `./*.html`])
+      .on('change', options.browserSync.reload);
+
+    if (files.length > 0) {
+      gulp.watch(files, gulp.series(options.tasks.buildSassFiles));
+    }
+  };
+};
